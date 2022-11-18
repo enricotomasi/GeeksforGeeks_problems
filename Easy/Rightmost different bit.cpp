07b1fc1af177397e6e1f79251af38581a02776c9
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2022-11-18 20:01:55 +0100

    commit

---
+++ Easy/Rightmost different bit.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        int ans = 1;
+        
+        while (m > 0 || n > 0)
+        {
+            if (n%2 != m%2) return ans;
+            ans++;
+            m>>=1;
+            n>>=1;
+        }
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
