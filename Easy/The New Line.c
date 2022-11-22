48e6b1b458f4b33aaa5f97bad9a32c95bf25ef70
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2022-11-22 14:23:00 +0100

    Create The New Line.c

---
+++ Easy/The New Line.c
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+
+void printNewLine();
+
+
+// } Driver Code Ends
+//User function Template for C
+
+// Function to print each word in new line
+void printNewLine()
+{
+    // Your code here
+    printf("Geeks\nfor\nGeeks\n");
+    
+    
+    
+}
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    //function call
+    printNewLine();
+    
+}
+// } Driver Code Ends
