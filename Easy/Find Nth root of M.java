80a2e685f49bd9b819a8d31957718835f997790c
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2022-11-27 11:49:46 +0100

    Create Find Nth root of M.java

---
+++ Easy/Find Nth root of M.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            String[] S1 = s.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            Solution ob = new Solution();
+            int ans = ob.NthRoot(n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int NthRoot(int n, int m)
+    {
+        // code here
+        double rad = (double)1 / (double)n;
+	    double sqr = Math.pow((double)m, rad);
+
+        if (Math.abs((int)Math.round(sqr) - sqr) > 0.00000001) return -1;
+        else return (int)Math.round(sqr);
+        
+    }
+}
