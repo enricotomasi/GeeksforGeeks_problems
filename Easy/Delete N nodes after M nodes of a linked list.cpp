a39b873695d9cfb98b0fb9dfcae7b7eb994c3979
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2022-12-03 10:35:49 +0100

    Create Delete N nodes after M nodes of a linked list.cpp

---
+++ Easy/Delete N nodes after M nodes of a linked list.cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while(node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert(int n1)
+{
+    int n,value;
+    n=n1;
+    struct Node *temp;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp=start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp=temp->next;
+        }
+    }
+}
+
+// } Driver Code Ends
+/*
+delete n nodes after m nodes
+  The input list will have at least one element  
+  Node is defined as 
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+*/
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int M, int N)
+    {
+        //Add code here   
+        
+        int conta = 1;
+        
+        while(head != NULL && head->next != NULL)
+        {
+            if(conta == M)
+            {
+                conta = 1;
+                int n2 = N;
+                while(head->next != NULL && n2 > 0)
+                {
+                    n2--;
+                    Node *temp = head->next;
+                    head->next = head->next->next;
+                    delete temp;
+                }
+            }
+            else conta++;
+            head = head->next;
+        }
+   
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n1;
+    cin>>t;
+    while (t--) {
+        cin>>n1;
+        int m,n;
+        cin>>m;
+        cin>>n;
+        insert(n1);
+        Solution ob;
+        ob.linkdelete(start,m,n);
+        printList(start);
+    }
+    
+    return 0;
+}
+
+// } Driver Code Ends
