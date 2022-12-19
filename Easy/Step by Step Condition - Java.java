c9f1c192f1793af4d2db4620dd75427a8bb51f7a
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2022-12-19 23:20:38 +0100

    Create Step by Step Condition - Java.java

---
+++ Easy/Step by Step Condition - Java.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases count
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking n
+		    int n =sc.nextInt();
+		
+	        //creating an object of class Geeks	
+		    Geeks obj=new Geeks();
+		    
+		    //calling isDivisbleByPrime()
+		    //method of class Geeks
+		    obj.isDivisibleByPrime(n);
+		}
+	
+		
+	}
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+//Back-end complete function Template for Java
+class Geeks
+{
+	 static void isDivisibleByPrime (int n) 
+	 {
+        //Your code here
+        String ans = "-1";
+        
+        if      (n%11 == 0) ans = "Eleven";
+        else if (n%3 == 0)  ans = "Three";
+        else if (n%2 == 0)  ans = "Two";
+        
+        System.out.println(ans);
+	 }
+
+}
+
+//{ Driver Code Starts.
+
+/*package whatever //do not write package name here */
+// } Driver Code Ends
