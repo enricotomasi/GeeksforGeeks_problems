2a6295a3a4ac9042b145b28c81267f90515b7a94
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-01-06 10:54:30 +0100

    Create Vertical sum.cpp

---
+++ Easy/Vertical sum.cpp
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+int data;
+Node *left,*right;
+};
+*/
+
+class Solution
+{
+  public:
+  
+    /*
+        When move left , decrement level by 1. When move right, increment level by 1. 
+        Store node's sum in respective level.
+    */
+  
+    void ric(Node* root, map<int, int> &mappa, int livello)
+    {
+        if (root == NULL) return;
+        mappa[livello] += root->data;
+        
+        ric(root->left,  mappa, livello-1);
+        ric(root->right, mappa, livello+1);
+    }
+  
+    vector <int> verticalSum(Node *root) 
+    {
+        // add code here.
+        vector<int> ans;
+        map<int, int> mappa;
+        ric (root, mappa, 1);
+        
+        if (mappa.size() <= 0)
+        {
+            ans.push_back(-1);
+            return ans;
+        }
+        
+        for (auto it : mappa)
+        {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        Solution obj;
+        vector <int> res = obj.verticalSum(root);
+        for (int i : res) cout << i << " ";
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
