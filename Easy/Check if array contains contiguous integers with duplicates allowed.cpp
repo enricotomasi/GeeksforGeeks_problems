05ad86759120cc0cbe1538396ce7cb0891eece48
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-01-13 19:29:55 +0100

    Create Check if array contains contiguous integers with duplicates allowed.cpp

---
+++ Easy/Check if array contains contiguous integers with duplicates allowed.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// C++ implementation to check whether the array
+// contains a set of contiguous integers
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    // Function to check whether the array contains
+    // a set of contiguous integers
+    bool areElementsContiguous(int arr[], int n)
+    {
+	    // Complete the function
+	    sort(arr, arr+n);
+
+	    int last = arr[0];
+	    for (int i=1; i<n; i++)
+	    {
+	       // cout << arr[i] << " " << last << endl;
+	       if (arr[i] != last &&
+	           arr[i] != last+1)  return false;
+	       
+	       last = arr[i];
+	    }
+	    
+	    return true;
+	    
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n + 1];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        if (ob.areElementsContiguous(arr, n))
+            cout << "Yes" <<endl;
+        else
+            cout << "No" <<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
