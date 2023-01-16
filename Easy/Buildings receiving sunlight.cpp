a22bc0b7ed4b8b0e340320ce1925dfd01e39323d
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-01-16 19:18:31 +0100

    Create Buildings receiving sunlight.cpp

---
+++ Easy/Buildings receiving sunlight.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    int longest(int arr[],int n)
+    {
+        // Write your code here
+        int massimo = 0;
+        int ans = 0;
+        
+        for (int i=0; i<n; i++)
+        {
+            int palazzo = arr[i];
+            if (palazzo >= massimo) ans++;
+            massimo = max(massimo, palazzo);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int i, arr[n+5];
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        Solution ob;
+        cout<<ob.longest( arr, n )<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
