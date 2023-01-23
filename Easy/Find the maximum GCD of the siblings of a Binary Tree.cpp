6040731fd4f23177cbc5d58539438b0ec37b2d8a
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-01-23 22:33:45 +0100

    Create Find the maximum GCD of the siblings of a Binary Tree.cpp

---
+++ Easy/Find the maximum GCD of the siblings of a Binary Tree.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; 
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+
+class Solution
+{
+    public:
+    int gcd(int a, int b)
+    {
+        if (b==0) return a;
+        return gcd(b, a%b);
+    }
+    
+    void ric(Node *root, int &g, int &nodo)
+    {
+        if (!root) return;
+        
+        int mcd = 0;
+        
+        if      (root->left && root->right) mcd = gcd(root->left->data, root->right->data);
+        //else if (root->left)                mcd = root->left->data;
+        //else if (root->right)               mcd = root->right->data;
+        
+        if (mcd >0 && mcd > g || (mcd >0 && mcd == g && nodo < root->data))
+        {
+            g = mcd;
+            nodo = root->data;
+        }
+        
+        //cout << endl;
+        //cout << "Nodo: " << root->data << endl;
+        //if (root->left)  cout << "Sinistra: " << root->left->data  << endl;
+        //if (root->right) cout << "Destra  : " << root->right->data << endl;
+        //cout << "mcd: " << mcd << endl;
+        
+        ric(root->left,  g, nodo);
+        ric(root->right, g, nodo);
+    }
+
+
+    int maxGCD(Node* root)
+    {
+        //code here
+        int g = 0;
+        int nodo = 0;
+        ric(root, g, nodo);
+        return nodo;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string treeString;
+		getline(cin,treeString);
+		Node* root = buildTree(treeString);
+		Solution ob;
+		cout<<ob.maxGCD(root)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
