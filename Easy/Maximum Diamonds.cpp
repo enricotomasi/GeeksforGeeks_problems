9eb38d0a63c133f18865aeea17145e88370e9c49
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-03-20 21:22:03 +0100

    Create Maximum Diamonds.cpp

---
+++ Easy/Maximum Diamonds.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution 
+{
+  public:
+    int maxDiamonds(int A[], int N, int K) 
+    {
+        // code here
+        priority_queue<int>pq;
+        
+        for (int i=0; i<N; i++)
+        {
+            pq.push(A[i]);
+        }
+        
+        int ans = 0;
+        
+        for (int i=0; i<K; i++)
+        {
+            int x = pq.top();
+            pq.pop();
+            ans += x;
+            x /= 2;
+            pq.push(x);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+        
+        cin>>N>>K;
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.maxDiamonds(A,N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
