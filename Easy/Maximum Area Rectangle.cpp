68fd60058d4541cb6eb2b85f03251546a87db2b4
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-03-22 22:30:03 +0100

    Create Maximum Area Rectangle.cpp

---
+++ Easy/Maximum Area Rectangle.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int calculate_Area(vector<pair<int, int>>&rect, int N)
+    {
+        // Your code goes here
+        int ans = 0;
+        
+        for (auto it : rect)
+        {
+            int area = it.first * it.second;
+            ans = max(ans, area);
+        }
+        
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<pair<int,int>>rect;
+        for(int i = 0 ; i < n; i++){
+            int a, b;
+            cin >> a >> b;
+            rect.push_back({a, b});
+        }
+        Solution ob;
+        cout << ob.calculate_Area(rect, n) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
