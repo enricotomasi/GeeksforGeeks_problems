2896973adba2be74307121509e853ad1ef16686b
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-03-26 09:55:21 +0200

    Create Pattern 18.cpp

---
+++ Easy/Pattern 18.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    void printTriangle(int n) 
+    {
+        // code here
+
+        for (int i=0; i<n; i++)
+        {
+            char c = 'A' + n-1;
+            bool avanti = false;
+            for (int j=0; j<=i; j++)
+            {
+                cout << c << " ";
+                if (avanti)
+                {
+                    c++;
+                    if (c > 'Z')
+                    {
+                        avanti = true;
+                        c == 'A';
+                    }
+                }
+                else
+                {
+                    c--;
+                }
+            }
+            cout << endl;
+        }
+        
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
