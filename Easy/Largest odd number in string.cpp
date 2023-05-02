8f5380ad48b54afcb7145f6db796c26bcecb3acf
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-05-02 19:03:46 +0200

    Create Largest odd number in string.cpp

---
+++ Easy/Largest odd number in string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string maxOdd(string s)
+    {
+        // your code here
+        int n = s.length(); 
+        
+        for (int i=n-1; i>=0; i--)
+        {
+            //cout << (s[i] - '0') % 2 << endl;
+            if ((s[i] - '0') %2 != 0)
+            {
+                return s.substr(0, i+1);
+            }
+        }
+        
+        
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+
+        cout << ob.maxOdd(s) << endl;
+    }
+}
+
+// } Driver Code Ends
