33f089ab6533424f31d6ad792f424e09b66e56b6
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-10-20 13:50:39 +0200

    Create Range of Composite numbers.py

---
+++ Easy/Range of Composite numbers.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+import math
+
+
+def Range(n):
+    #return 1/0
+    #code here
+    
+    return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+
+    for _ in range(t):
+        n=int(input())
+
+        print(Range(n))
+# } Driver Code Ends
