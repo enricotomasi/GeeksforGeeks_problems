182683f28361a49d61db03fe5703ab4bec394d16
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-11-10 22:01:10 +0100

    Create Isomorphic Strings.py

---
+++ Easy/Isomorphic Strings.py
@@ -0,0 +1,57 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if two strings are isomorphic.
+    def areIsomorphic(self, str1, str2):
+        n = len(str1)
+        
+        if n != len(str2):
+            return 0
+        
+        d = dict()
+        v = set()
+        
+        for i in range(n):
+            if str1[i] in d:
+                if d[str1[i]] != str2[i]:
+                    return False
+            else:
+                if str2[i] in v:
+                    return False
+            
+            d[str1[i]] = str2[i]
+            v.add(str2[i])
+        
+        return True
+      
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import defaultdict
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        p=str(input())
+        ob = Solution()
+        if(ob.areIsomorphic(s,p)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
