fc9b64cb201b546dd49388947349c3a38261571f
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2023-11-26 10:46:55 +0100

    Create Print Pattern 2.cpp

---
+++ Easy/Print Pattern 2.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    void rec(int n, vector<int>& ans)
+    {
+        if (n <= 0)
+        {
+            ans.push_back(n);
+            return;
+        }
+        
+        ans.push_back(n);
+        
+        rec(n - 5, ans);
+        
+        ans.push_back(n);
+    }
+    
+    vector<int> pattern(int N)
+    {
+        // code here
+        vector<int> ans;
+        rec (N, ans);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.pattern(N);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
