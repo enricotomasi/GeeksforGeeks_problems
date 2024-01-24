a373120518bdc1eb0cc6d98447f2675cb9a93131
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-01-24 11:05:44 +0100

    Create Factorials of large numbers.py

---
+++ Medium/Factorials of large numbers.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+    def factorial(self, N):
+        #code here
+        fact = 1
+        
+        for i in range(1, N+1):
+            fact *= i
+        
+        
+        ans = []
+        
+        s = str(fact)
+        
+        for c in s:
+            ans += [(ord(c) - ord("0"))]
+        
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorial(N);
+        for i in ans:
+            print(i,end="")
+        print()
+    
+# } Driver Code Ends
