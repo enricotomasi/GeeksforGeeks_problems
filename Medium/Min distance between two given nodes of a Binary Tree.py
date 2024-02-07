37b795f9686af70d8caf387baea3e7411e2594da
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-02-07 11:28:30 +0100

    Create Min distance between two given nodes of a Binary Tree.py

---
+++ Medium/Min distance between two given nodes of a Binary Tree.py
@@ -0,0 +1,148 @@
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    def findDist(self, root, a, b):
+    
+        #return: minimum distance between a and b in a tree with given root
+        #code here
+        def dist(root, toFind, temp):
+            if not root:
+                return -1
+                
+            if root.data == toFind:
+                return temp
+                
+            l = dist(root.left, toFind, temp + 1)
+            
+            if l != -1:
+                return l
+                
+            return dist(root.right, toFind, temp + 1)
+            
+        def findAnc(root, a, b):
+            if root is None:
+                return root
+            
+            if root.data == a or root.data == b:
+                return root
+            
+            l = findAnc(root.left, a, b)
+            r = findAnc(root.right, a, b)
+            
+            if l and r:
+                return root
+            
+            
+            if l is not None:
+                return l
+            
+            return r
+            
+
+        anc = findAnc(root, a, b)
+        
+        da = dist(anc, a, 0)
+        db = dist(anc, b, 0)
+        
+        return da + db
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(50000)
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        a, b=map(int, input().split())
+        ob = Solution()
+        print(ob.findDist(root, a, b))
+
+# } Driver Code Ends
