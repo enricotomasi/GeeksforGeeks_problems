c8610e803accbdfbc889af70a0dd03e0489c152b
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-02-27 09:19:24 +0100

    Update Play With OR.py

--- Easy/Play With OR.py
+++ Easy/Play With OR.py
@@ -6,13 +6,38 @@ class Solution:
         if n < 1:
             return arr
         
-        last = arr[0]
+        for i in range(n - 1):
+            arr[i] = arr[i] | arr[i + 1]
+        
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    ob = Solution()
+    res = ob.game_with_number(arr, n);
+    print(*res)
+
+
+
+
+# } Driver Code Ends#User function Template for python3
+
+class Solution:
+    def game_with_number (self, arr,  n) : 
+        #Complete the function
+        if n < 1:
+            return arr
         
         for i in range(n - 1):
-            new = arr[i] | arr[i + 1]
-            last = arr[i]
-            
-            arr[i] = new
+            arr[i] = arr[i] | arr[i + 1]
         
         return arr
 
