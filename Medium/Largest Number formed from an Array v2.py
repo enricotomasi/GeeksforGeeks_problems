11699d0b65a22fe36fbe6dd5bf010660f4031ccd
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-03-03 10:26:15 +0100

    Create Largest Number formed from an Array v2.py

---
+++ Medium/Largest Number formed from an Array v2.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+from functools import cmp_to_key
+
+class Solution:
+    def cmp(self, a, b):
+        if a + b > b + a:
+            return -1
+        return 1
+
+	def printLargest(self, n, arr):
+	    # code here
+	    for it in enumerate(arr):
+            arr[it[0]] = str(it[1])
+    
+        arr = sorted(arr, key = cmp_to_key(self.cmp))
+    
+        ans = ""
+        
+        for it in arr:
+            ans += str(it)
+
+        return ans
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import functools
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(str, input().strip().split()))
+        ob = Solution()
+        ans = ob.printLargest(n, arr)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
