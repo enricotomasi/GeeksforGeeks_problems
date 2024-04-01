71adffa2fadb77609e4e09394b941b221b02e0db
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-04-01 13:31:15 +0200

    Create Pairs violating the BST property.cpp

---
+++ Medium/Pairs violating the BST property.cpp
@@ -0,0 +1,174 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Tree node
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Utility function to create a new node
+Node* newNode(int data) {
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return (temp);
+}
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*// A Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};*/
+
+class Solution
+{
+  public:
+
+    void inord(Node *root, vector<int> &ans)
+    {
+        if (!root) return;
+        
+        inord(root->left, ans);
+        ans.push_back(root->data);
+        inord(root->right, ans);
+    }
+    
+    /*You are required to complete below function */
+    int pairsViolatingBST(int n, Node *root)
+    {
+        // your code goes here
+        vector<int> inorder;
+        inord(root, inorder);
+        
+        //for (auto it : inorder) cout << it << " ";
+        //cout << endl;
+        
+        int ni = inorder.size();
+        int ans = 0;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        for (int i = 0; i < ni; i++)
+        {
+            pq.push(make_pair(inorder[i], i));
+        }
+        
+        vector<int> ind;
+        
+        while (!pq.empty())
+        {
+            int temp = pq.top().second;
+            
+            pq.pop();
+            
+            int y = upper_bound(ind.begin(), ind.end(), temp) - ind.begin();
+            
+            ans += temp - y;
+            
+            ind.insert(upper_bound(ind.begin(), ind.end(), temp), temp);
+            
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    getchar();
+
+    while (t--) {
+
+        int n;
+        cin >> n;
+        getchar();
+
+        string inp;
+        getline(cin, inp);
+
+        struct Node* root = buildTree(inp);
+
+        Solution ob;
+        int ans = ob.pairsViolatingBST(n, root);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
