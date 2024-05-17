c83e2b57fe036ed42febec900b500e118a11627d
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-05-17 10:42:26 +0200

    Create Find Pair Given Difference.py

---
+++ Easy/Find Pair Given Difference.py
@@ -0,0 +1,57 @@
+
+from typing import List
+
+
+class Solution:
+    def findPair(self, n : int, x : int, arr : List[int]) -> int:
+        # code here
+        arr.sort() 
+        start = 0
+        end = 1
+        
+        while start < n and end < n:
+            if start != end and arr[end] - arr[start] == x:
+                return 1
+            
+            if arr[end] - arr[start] < x:
+                end += 1
+            else:
+                start += 1
+        
+        return -1
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        x = int(input())
+
+        arr = IntArray().Input(n)
+
+        obj = Solution()
+        res = obj.findPair(n, x, arr)
+
+        print(res)
+
+# } Driver Code Ends
